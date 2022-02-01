ab4ab669bc573b86b9e4bb8fa9ccb4d239a9bd24
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-01 15:30:18 -0500

    Added Exception Handling & Data Validation

--- FutureValueProject/Form1.cs
+++ FutureValueProject/Form1.cs
@@ -14,8 +14,34 @@ namespace FutureValueProject {
             InitializeComponent();
         }
 
+        private string IsNotBlank(string value, string name) {
+            if(value == string.Empty){ // Checks if string is empty
+                return $"{name} is required!\n";
+            }
+            return string.Empty;
+        }
+
         private void btnCalculate_Click(object sender, EventArgs e) {
-            decimal monthlyInvestment = Convert.ToDecimal(txtMonthlyInvestment.Text);
+            try {
+                // Checks the 3 inputted values to make sure have entries entered
+                string message = IsNotBlank(txtMonthlyInvestment.Text, "Monthly Investment");
+                message += IsNotBlank(txtInterestRate.Text, "Interest Rate");
+                message += IsNotBlank(txtYears.Text, "Years");
+
+                decimal monthlyInvestment = Convert.ToDecimal(txtMonthlyInvestment.Text);
+                if(monthlyInvestment <= 0 || monthlyInvestment >= 10000) {
+                    message += "Monthly Investment must be between 1 and 9999!";
+                }
+
+                if (message != string.Empty){
+                    MessageBox.Show(message, "Entry Error");
+                    return;
+                }
+
+            //// Running below 2 lines causes a divide by 0 error to occur when enter a value
+            //int d = 0;
+            //int z = 1 / d;
+
             decimal yearlyInterestRate = Convert.ToDecimal(txtInterestRate.Text);
             int years = Convert.ToInt32(txtYears.Text);
 
@@ -23,13 +49,31 @@ namespace FutureValueProject {
             decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
 
             decimal futureValue = 0m;
-            for (int i=0; i < months; i++)
-            {
+            for (int i=0; i < months; i++){
                 futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
             }
 
             txtFutureValue.Text = futureValue.ToString("c");
             txtMonthlyInvestment.Focus();
+            }
+            catch(FormatException){
+                MessageBox.Show(
+                    "Invalid numeric format. Please check all entries.",
+                    "Entry Error"
+                    );
+            }
+            catch(OverflowException) {
+                MessageBox.Show(
+                    "Overflow Error. Please enter smaller values",
+                    "Entry Error"
+                    );
+            }
+            catch(Exception ex) {
+                MessageBox.Show(
+                    ex.Message,
+                    ex.GetType().ToString()
+                    );
+            }
         }
 
         private void btnExit_Click(object sender, EventArgs e) {
